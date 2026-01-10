7e787ceca250df526898b39dd51395f844598771
Janis Rolf <janis.rolf@stud.uni-goettingen.de>
2026-01-10 11:53:20 +0100

    added Error structs finished

--- src/Error/Error.h
+++ src/Error/Error.h
@@ -2,9 +2,14 @@
 
 #include <string_view>
 #include <string>
+#include <type_traits>
+#include <variant>
 
-#define CURRENT_LOCATION \ Error::SourceLocation{__FILE__, __func__, __LINE__}
-#define MAKE_ERROR(code, msg) \ Error::Error{ code, msg, CURRENT_LOCATION}
+#include "Errorcodes.h"
+
+#define CURRENT_LOCATION ::Error::SourceLocation{__FILE__, __func__, __LINE__}
+
+#define MAKE_ERROR(code, msg) ::Error::make_error(code, msg)
 
 namespace Error {
 
@@ -14,29 +19,40 @@ namespace Error {
         int line;
     };
 
-    //mit error typre(wie? muss eig enum sein) --> und muss dass dann nicht in der typdef deklariert sein oder ist das bei classen egal??
-    template<enum E> 
-    struct Error
+    template<typename E> 
+    struct ErrorValue
     {
+        static_assert(std::is_enum<E>, "Error code musst be an enum Type");
+
         E ErrorCode;
         std::string_view Message;
         SourceLocation Location;
     };
 
-    template<typename T>
+    template<typename E>
+    ErrorValue<E> make_error(E code, std::string_view msg){
+        return { code, msg, CURRENT_LOCATION };
+    }
+
+    template<typename value_Type, typename E>
     class Result {
-    private:
-        bool isOk;
-
-        union{
-            T m_Result;
-            Error m_ErrResult;
-        }
     public:
-        //is OK
-        //getError
-        //getResult u.s.w
+        using ErrorType = ErrorValue<E>;
+    private:
+        std::variant<value_Type, ErrorType> m_data;
+    public: 
+        Result(const value_Type& value) : m_data(value) {}
+        Result(value_Type&& value) : m_data(std::move(value)) {}
+        Result(const ErrorType& error) : m_data(error) {}
+        Result(ErrorType&& error) : m_data(std::move(error)) {}
 
+        bool isOK() const { return std::holds_alternative<value_Type>(m_data); }
+        bool isErr() const {return !isOK(); }
+
+        explicit operator bool() const { return ok(); }
+
+        const value_Type& value() { return std::get<value_Type> (m_data); }
+        const ErrorType& error() { return std::get<ErrorType> (m_data); }
 
     };
 
--- src/Error/Errorcodes.h
+++ src/Error/Errorcodes.h
@@ -5,9 +5,4 @@ namespace http {
     enum HTTPErrors {
 
     };
-
-    enum ErrorCodes {
-        
-    };
-
 }
